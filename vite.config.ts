d93ca8c1996ec5d8def8e53863a358e7319079b3
hoang0410 <168557453+hoang0410@users.noreply.github.com>
2025-10-20 12:44:02 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/chuc-mung-20-10-chi-Oanh",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
